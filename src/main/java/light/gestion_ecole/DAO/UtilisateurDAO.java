e75e19fb35da9b752feb4cabb2f65bd4eda1a508
Arikanto2 <fahazavanaandriantsoa@gmail.com>
2025-09-15 09:07:25 +0300

    Import et export

--- src/main/java/light/gestion_ecole/DAO/UtilisateurDAO.java
+++ src/main/java/light/gestion_ecole/DAO/UtilisateurDAO.java
@@ -2,26 +2,38 @@ package light.gestion_ecole.DAO;
 
 import light.gestion_ecole.Model.Utilisateur;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UtilisateurDAO {
-    public static String Connecter(Utilisateur utilisateur) throws SQLException {
-        String sql = "SELECT * FROM utilisateur WHERE nomutilisateur = ? AND mdp = ?";
+    public static Utilisateur Connecter(String mdp, String nom) throws SQLException {
+        String sql = "SELECT emailutil FROM utilisateur WHERE nomutilisateur = ? AND mdp = ?";
         try(Connection connect = Database.connect();
             PreparedStatement stmt = connect.prepareStatement(sql);){
-            stmt.setString(1,utilisateur.getEmail());
-            stmt.setString(2,utilisateur.getMDP());
+            stmt.setString(1,nom);
+            stmt.setString(2,mdp);
             try(ResultSet rs = stmt.executeQuery()){
                 if (rs.next()){
-                    String nom = rs.getString("nomutilisateur");
-                    return nom;
+                    String mail = rs.getString("emailutil");
+                    Utilisateur u = new Utilisateur(mail,nom);
+                    return u;
                 }else {
-                    return "";
+                    return null;
                 }
             }
         }
+    }
+    public static List<String> getEmailUtilisateur() throws SQLException {
+        List<String> emailUtilisateur = new ArrayList<>();
+        String sql = "SELECT emailutil FROM utilisateur ";
+        try(Connection connect = Database.connect();
+            Statement stmt = connect.createStatement();){
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()){
+            emailUtilisateur.add(rs.getString("emailutil"));}
+        }
+        return   emailUtilisateur;
+
     }
 }
